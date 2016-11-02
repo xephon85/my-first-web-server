b5829f4a532a263870d377eb7179c8b68fc0add0
Colin Clarkson <colinsclarkson@gmail.com>
2016-11-01 19:33:58 -0500

    completed app

--- my-first-express-app/app.js
+++ my-first-express-app/app.js
@@ -18,10 +18,12 @@ app.get('/', function(req, res, next) {
    greeting: 'Hi! My name is ',
    name: 'Colin.',
    intro: 'I live in my house, and love speghetti!',
-   likes: ['Games', 'Movies', 'Books', 'Science']
+   img: 'https://scontent.xx.fbcdn.net/v/t1.0-9/12193549_10153770533183258_6058267893669094163_n.jpg?oh=9dc31ebd4002203073b054439ace7f59&oe=5895D97A',
+   likes: ['Games', 'Movies', 'Books', 'Science'],
+   dislikes: ['The number 7', 'A florishy shade of Yellow', 'Turtles that go "wonk rooooooo!"', 'People with faces']
  });
 });
 
 // set up our server
-const port = 3000;
+const port = 3001;
 app.listen(port, () => console.log(`Server listening on: ${port}`));
